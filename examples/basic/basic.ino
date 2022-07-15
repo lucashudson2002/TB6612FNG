e6cf66605cd813cb0c11e5de2fa8ba315d8943a8
Lucas Dias Hudson <72749364+lucashudson2002@users.noreply.github.com>
2022-07-15 10:13:42 -0300

    Update basic.ino

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -8,20 +8,26 @@
 #define IN1B 7
 #define IN2B 8
 
+//Create the object for TB6612FNG with the define above
 TB6612FNG robot (IN1A, IN2A, IN1B, IN2B, PWMA, PWMB, STBY);
+//TB6612FNG robot (IN1A, IN2A, IN1B, IN2B, PWMA, PWMB);
+//TB6612FNG robot (IN1A, IN2A, IN1B, IN2B, STBY);
+//TB6612FNG robot (IN1A, IN2A, IN1B, IN2B);
 
 void setup() {
+  //initialize the H-bridge and pinMode them
   robot.begin();
+  //set PWM to the maximum possible
   robot.set_pwm(255);
 }
 
 void loop() {
-  robot.forward();
+  robot.forward(); //robot goes forward
   delay(1000);
-  robot.stop();
+  robot.stop(); ; //robot stops
   delay(1000);
-  robot.backward();
+  robot.backward(); //robot goes backward
   delay(1000);
-  robot.stop();
+  robot.brake(); ; //robot brakes
   delay(1000);
 }
